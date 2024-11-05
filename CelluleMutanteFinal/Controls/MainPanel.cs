b7655dc7a8969846e2f7d7850560d5d1b1553868
ChaiseEnFer <maxime.brunel.lycee@gmail.com>
2024-11-05 14:54:19 +0100

    ajout de plein de classe plus ajout dans le main

---
+++ CelluleMutanteFinal/Controls/MainPanel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace CelluleMutanteFinal.Controls
+{
+    public class MainPanel : Panel
+    {
+        public MainPanel()
+        {
+            Name = "pnl_main";
+            BackColor = Color.LightGray;
+            Anchor = AnchorStyles.None;
+            Size = new Size(400, 400);
+            Dock = DockStyle.None;
+
+        }
+
+
+    }
+}
--- CelluleMutanteFinal/Form1.cs
+++ CelluleMutanteFinal/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CelluleMutanteFinal.Controls;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,53 @@ namespace CelluleMutanteFinal
 {
     public partial class Form1 : Form
     {
+        Panel pnl_main;
+        Button btn_simulation;
+        Cell cell;
+
         public Form1()
         {
             InitializeComponent();
+            // Initialisation d’un Panel en utilisant notre classe
+            pnl_main = new MainPanel();
+            pnl_main.Location = new Point((Size.Width - pnl_main.Width) / 2 - 10,
+            (Size.Height - pnl_main.Height) / 2 - 40);
+            pnl_main.Anchor = AnchorStyles.None;
+            // Initialisation d’un Button en utilisant notre classe
+            btn_simulation = new SimulationButton();
+            btn_simulation.Location = new Point(90, 390);
+            btn_simulation.Anchor = AnchorStyles.None;
+            btn_simulation.Anchor = AnchorStyles.None;
+            btn_simulation.Click += new EventHandler(btn_simulation_Click);
+            // Ajout des éléments à notre fenêtre
+            Controls.Add(pnl_main);
+            Controls.Add(btn_simulation);
+            pnl_main.Anchor = AnchorStyles.None;
+            pnl_main.Paint += new PaintEventHandler(pnl_main_Paint);
+            //Cell
+            cell = new Cell();
+
         }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Définir la taille et le titre de la fenêtre
+            Size = new Size(500, 500);
+            Text = "Cellule Mutante";
+        }
+
+        private void pnl_main_Paint(object sender, PaintEventArgs e)
+        {
+            var g = e.Graphics;
+            g.Clear(pnl_main.BackColor);
+            SolidBrush coloredBrush = new SolidBrush(Color.Black);
+            g.FillEllipse(coloredBrush, pnl_main.Width / 2, pnl_main.Width / 2, 10, 10);
+            g.Dispose();
+           
+        }
+        private void btn_simulation_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("La simulation commence");
+        }
+
     }
 }
